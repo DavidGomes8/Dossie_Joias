d3068ce31dbba65bfdf26e7919b4d86645a51894
DavidGomes8 <43244696+DavidGomes8@users.noreply.github.com>
2019-04-28 23:17:07 -0300

    Initial commit

---
+++ Assets/MyAssets/Script/Toque.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toque : MonoBehaviour
+{
+
+    public float distancia = 1000000f;
+
+    public GameObject[] joias = new GameObject[6];
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        toque();
+        
+    }
+
+    private void toque()
+    {
+        if (Input.GetMouseButton(0) && joias[0].gameObject.GetComponent<Collider>().enabled)
+        {
+            Vector3
+        }
+
+        if (Input.GetMouseButton(0) && joias[1].gameObject.GetComponent<Collider>().enabled)
+        {
+            
+        }
+
+        if (Input.GetMouseButton(0) && joias[2].gameObject.GetComponent<Collider>().enabled)
+        {
+            
+        }
+
+        if (Input.GetMouseButton(0) && joias[3].gameObject.GetComponent<Collider>().enabled)
+        {
+            
+        }
+
+        if (Input.GetMouseButton(0) && joias[4].gameObject.GetComponent<Collider>().enabled)
+        {
+            
+        }
+
+        if (Input.GetMouseButton(0) && joias[5].gameObject.GetComponent<Collider>().enabled)
+        {
+            
+        }
+    }
+}
